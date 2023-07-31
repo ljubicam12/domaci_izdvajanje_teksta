71a7f5b090c73ef8bed02429b303c5840159b6b7
ljubicam12 <131596865+ljubicam12@users.noreply.github.com>
2023-07-31 19:10:51 +0200

    Add files via upload

--- domaci.py
+++ domaci.py
@@ -13,8 +13,8 @@ def brightness(img):
     mean_brightness=cv2.mean(img)[0]
     return mean_brightness
 
-def estimate_background_illumination(img):
-    kernel=np.ones((5,5), np.uint8)
+def estimate_background_illumination(img, kernel_size):
+    kernel=np.ones((kernel_size,kernel_size), np.uint8)
     opening=cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
     closing=cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
     blur=cv2.medianBlur(closing, 21)
@@ -29,8 +29,8 @@ def image_sharpening(img):
 
 def image_binarization(img, tresh):
     for i, row in enumerate(img):
-        for j, pixel in enumerate(img):
-            if(j-tresh<tresh/2):
+        for j, pixel in enumerate(row):
+            if(img[i][j]>225):
                 img[i][j]=0
             else:
                 img[i][j]=255
@@ -43,12 +43,28 @@ def image_binarization(img, tresh):
 
 
 img=cv2.imread("C:\slike\Tekst_1.png")
-img_gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_gray=cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 tresh=brightness(img_gray)
 
-end_img=image_binarization(image_sharpening(estimate_background_illumination(img_gray)), tresh)
 
-plt.imshow(end_img, cmap="gray")
+image_brightness=estimate_background_illumination(img,100)
+#end_img=image_binarization(image_sharpening(estimate_background_illumination(img_gray)), tresh)
+img_gray2=cv2.cvtColor(image_brightness, cv2.COLOR_RGB2GRAY)
+
+new_img=img-image_brightness
+
+histr=cv2.calcHist(img,[0], None, [255],[0, 255])
+fig=plt.figure(figsize=(10,7))
+fig.add_subplot(2,2,1)
+plt.imshow(img, cmap='gray')
+fig.add_subplot(2,2,2)
+plt.imshow(new_img,cmap='gray')
+fig.add_subplot(2,2,3)
+plt.imshow(estimate_background_illumination(img,100))
+# fig.add_subplot(2,2,4)
+# plt.imshow(estimate_background_illumination(img, 200))
+
+print(img)
 plt.show()
 
 
